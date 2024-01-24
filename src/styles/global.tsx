0dbda89d08cdc87ebcf6c48eb96ac29f8386f2bd
littxx <ederlanbrito@gmail.com>
2024-01-24 00:23:10 -0300

    hello

---
+++ src/styles/global.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const stylesGlobal = StyleSheet.create({
+  // Defina seus estilos globais aqui
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  // outros estilos globais
+});
+
+export default stylesGlobal;
